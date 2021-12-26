baebd40370f374b9ee06c6da6226d141b094cace
ozakione <29860391+OzakIOne@users.noreply.github.com>
2021-12-26 22:31:22 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# TwitterBookmarkMediaDownloader
+
+[Credits goes to FarisHijazi/TwitterDownloader](https://github.com/FarisHijazi/TwitterDownloader)
+
+## How to use
+
+❗ MAKE SURE TO UNTICK `Ask where to save each file before downloading` in <a href="chrome://settings/downloads">chrome settings</a> (idk where it is in firefox) ❗
+
+1. copy the content of index.js
+1. go to https://twitter.com/i/bookmarks
+1. press F12
+1. go into console if necessary
+1. paste into the devtool console
+1. profit
---
+++ index.js
@@ -0,0 +1,69 @@
+(async function () {
+  let urls = [];
+  let names = [];
+  document.body.scrollIntoView();
+
+  // css prop : currentSrc
+
+  const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+  // replace 360x360 with orig to have the original image
+  const normalizeUrl = (url) => url.replace(/\&name=[^&]+/, '&name=orig');
+
+  // always returns jpeg
+  const getExtension = (url) => new URL(url).searchParams.get('format');
+
+  const download = (url, fname) =>
+    fetch(url)
+      .then((resp) => resp.blob())
+      .then((blob) => {
+        let bloburl = window.URL.createObjectURL(blob);
+        let a = document.createElement('a');
+        a.style.display = 'none';
+        a.href = bloburl;
+        if (!fname) {
+          fname = new URL(url).pathname.split('/').pop();
+          fname += getExtension(url);
+        } else {
+          fname +=
+            '_' + new URL(url).pathname.split('/').pop() + getExtension(url);
+        }
+        a.download = fname;
+        document.body.appendChild(a);
+        a.click();
+        window.URL.revokeObjectURL(bloburl);
+      })
+      .catch(() => console.error(`${url} download failed`));
+
+  do {
+    // somehow without var script throw error when running in browser
+    var newImgs = Array.from(document.querySelectorAll('img')).filter(
+      (img) =>
+        /\.mp4|media/.test(img.src) && !urls.includes(normalizeUrl(img.src)),
+    );
+    for (const img of newImgs) {
+      // fname equals to author @name
+      let filename = '';
+      let url = normalizeUrl(img.src);
+      urls.push(url);
+      try {
+        filename = img
+          .closest('[data-testid="tweet"]')
+          .innerText.split('\n')
+          .slice(1, 2);
+        // '.' +
+        // getExtension(url);
+      } catch (e) {
+        // if there is an error in getting the author name, just use the img id as the filename
+        filename = '';
+      }
+      names.push(filename);
+      console.log(urls.length, url, filename);
+      img.scrollIntoView();
+      download(url, filename);
+      await sleep(700);
+    }
+  } while (newImgs.length);
+
+  console.log(Array.from(urls).join('\n'));
+})();
